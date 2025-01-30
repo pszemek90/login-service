88f7fb8ef411678353928954368385b15d50ee34
pszemek <przemyslaw.miekos@gmail.com>
2025-01-30 08:25:44 +0100

    initial commit

---
+++ src/main/java/com/prezcode/loginservice/model/TokenResponse.java
@@ -0,0 +1,36 @@
+package com.prezcode.loginservice.model;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class TokenResponse {
+    @JsonProperty("access_token")
+    private String accessToken;
+    @JsonProperty("refresh_token")
+    private String refreshToken;
+    @JsonProperty("id_token")
+    private String idToken;
+
+    public String getAccessToken() {
+        return accessToken;
+    }
+
+    public void setAccessToken(String accessToken) {
+        this.accessToken = accessToken;
+    }
+
+    public String getRefreshToken() {
+        return refreshToken;
+    }
+
+    public void setRefreshToken(String refreshToken) {
+        this.refreshToken = refreshToken;
+    }
+
+    public String getIdToken() {
+        return idToken;
+    }
+
+    public void setIdToken(String idToken) {
+        this.idToken = idToken;
+    }
+}
